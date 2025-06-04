60e05c9538c081008c4343312deb07f2e469af5a
Noah Cohn <noahc42@gmail.com>
2025-06-04 08:43:58 -0700

    Update App.tsx

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -24,6 +24,7 @@ import { FAQ } from './components/FAQ'
 import { Tensor } from '@xenova/transformers'
 import cv from '@techstark/opencv-js'
 import { StableDiffusionControlNetPipeline } from '../../../dist/pipelines/StableDiffusionControlNetPipeline';
+import * as tf from '@tensorflow/tfjs';
 
 const darkTheme = createTheme({
   palette: {
